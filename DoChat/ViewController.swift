89aab66c8a9b1f5acbb382b3a0a97b170f78a066
andreiRookie <andrey.idem@gmail.com>
2022-03-24 21:18:47 +0400

    Initial commit

---
+++ DoChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoChat
+//
+//  Created by Andrei on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
